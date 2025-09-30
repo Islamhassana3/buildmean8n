e6bf7e8f8e3d2c5efea9e4db315206dfd9dcd8d3
GitHubPod <nick@tryconvenient.com>
2025-09-30 23:13:45 +0000

    Configure ESLint and Prettier to match n8n's coding standards
    - Add .prettierrc.cjs with n8n's formatting rules (tabs, single quotes, printWidth 100)
    - Update eslint.config.mjs with n8n's strict TypeScript and code quality rules
    - Update format script to use n8n's prettier configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,18 +7,76 @@ import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
 import importPlugin from "eslint-plugin-import";
 import globals from "globals";
 
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import stylisticPlugin from "@stylistic/eslint-plugin";
+import unicornPlugin from "eslint-plugin-unicorn";
+import importPlugin from "eslint-plugin-import-x";
+import unusedImportsPlugin from "eslint-plugin-unused-imports";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import globals from "globals";
+
 const tsAndJsRules = {
-  "no-unused-vars": "off",
-  "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
-  "@typescript-eslint/consistent-type-imports": "warn",
-  "import/order": [
-    "warn",
-    {
-      "groups": [["builtin", "external"], ["internal"], ["parent", "sibling", "index"]],
-      "newlines-between": "always",
-      "alphabetize": { "order": "asc", "caseInsensitive": true }
-    }
-  ]
+	"no-unused-vars": "off",
+	"@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+	"@typescript-eslint/consistent-type-imports": "error",
+	"@typescript-eslint/consistent-type-exports": "error",
+	"@typescript-eslint/array-type": ["error", { default: "array-simple" }],
+	"@typescript-eslint/await-thenable": "error",
+	"@typescript-eslint/ban-ts-comment": ["error", { "ts-ignore": true }],
+	"@typescript-eslint/no-restricted-types": [
+		"error",
+		{
+			types: {
+				Object: {
+					message: "Use object instead",
+					fixWith: "object",
+				},
+				String: {
+					message: "Use string instead",
+					fixWith: "string",
+				},
+				Boolean: {
+					message: "Use boolean instead",
+					fixWith: "boolean",
+				},
+				Number: {
+					message: "Use number instead",
+					fixWith: "number",
+				},
+			},
+		},
+	],
+	"@typescript-eslint/no-void": ["error", { allowAsStatement: true }],
+	"@typescript-eslint/member-delimiter-style": [
+		"error",
+		{
+			multiline: {
+				delimiter: "semi",
+				requireLast: true,
+			},
+			singleline: {
+				delimiter: "semi",
+				requireLast: false,
+			},
+		},
+	],
+	"@stylistic/indent": "off", // Delegated to Prettier
+	"no-constant-binary-expression": "error",
+	"sort-imports": "off",
+	"import/order": [
+		"error",
+		{
+			"groups": [["builtin", "external"], ["internal"], ["parent", "sibling", "index"]],
+			"newlines-between": "always",
+			"alphabetize": { "order": "asc", "caseInsensitive": true }
+		}
+	],
+	"unused-imports/no-unused-imports": "error",
+	"unicorn/no-unnecessary-await": "error",
+	"unicorn/no-useless-promise-resolve-reject": "error",
+	"unicorn/filename-case": ["error", { case: "kebabCase" }],
 };
 
 export default [
@@ -41,9 +99,9 @@ export default [
     },
     plugins: {
       "@typescript-eslint": tsPlugin,
-      react: reactPlugin,
-      "react-hooks": reactHooksPlugin,
-      jsxA11y: jsxA11yPlugin,
+      "@stylistic": stylisticPlugin,
+      unicorn: unicornPlugin,
+      "unused-imports": unusedImportsPlugin,
       import: importPlugin
     },
     rules: {
@@ -65,5 +123,6 @@ export default [
         }
       }
     }
-  }
+  },
+  eslintConfigPrettier
 ];
